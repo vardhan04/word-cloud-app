7ab2267aa90add460d13cd2f07e3ade46ae6456b
vardhan04 <saivardhan18@gmail.com>
2025-11-01 20:34:33 -0600

    docs: add comprehensive project documentation and polish

---
+++ frontend/.vite/deps/react-dom_client.js
@@ -0,0 +1,8 @@
+import {
+  require_client
+} from "./chunk-FRMXMRQ2.js";
+import "./chunk-GT3DDAAK.js";
+import "./chunk-32E4H3EV.js";
+import "./chunk-G3PMV62Z.js";
+export default require_client();
+//# sourceMappingURL=react-dom_client.js.map
---
+++ frontend/.vite/deps/react_jsx-runtime.js
@@ -0,0 +1,7 @@
+import {
+  require_jsx_runtime
+} from "./chunk-IHRST5LR.js";
+import "./chunk-32E4H3EV.js";
+import "./chunk-G3PMV62Z.js";
+export default require_jsx_runtime();
+//# sourceMappingURL=react_jsx-runtime.js.map
